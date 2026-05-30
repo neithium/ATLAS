0eb8103f845cd622a88b050bb48ffcff87ca837a
jnanaprasad1903 <jnanaprasadjp19@gmail.com>
2026-05-30 10:08:32 +0530

    updated config settings regarding kafka brokers

--- cluster.bat
+++ cluster.bat
@@ -15,10 +15,12 @@ echo Configuring 3-Broker KRaft cluster environment...
 set KAFKA_QUORUM_VOTERS=1@broker1:9093,2@broker2:9093,3@broker3:9093
 set KAFKA_REPLICATION_FACTOR=3
 set KAFKA_MIN_ISR=2
+set KAFKA_BOOTSTRAP=broker1:9092,broker2:9092,broker3:9092
 
 echo.
 echo Launching cluster containers...
 docker-compose --profile full-cluster up -d broker1 broker2 broker3 kafka-init
+docker-compose up -d --force-recreate atlas-ingestion
 
 echo.
 echo =======================================================
--- single.bat
+++ single.bat
@@ -3,7 +3,10 @@ echo Stopping any running Kafka brokers...
 docker-compose --profile full-cluster stop broker1 broker2 broker3 kafka-init
 docker-compose --profile full-cluster rm -f broker1 broker2 broker3 kafka-init
 
+set KAFKA_BOOTSTRAP=broker1:9092
+
 echo Starting Single Kafka Broker...
 docker-compose up -d broker1 kafka-init
+docker-compose up -d --force-recreate atlas-ingestion
 
 echo Done! Run 'docker ps' to check status.
